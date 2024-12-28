0c5a249bfbc96645386c28732ed9fa2e43f0ab06
loknezmonzter <175721383+lokNezMonzter@users.noreply.github.com>
2024-12-28 18:35:34 +0530

    Section 2: Removed setTimeout() function from homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,7 @@
 import ProductList from "@/components/custom/product/product-list";
 import sampleData from "@/mock/db/sample-data";
 
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default async function Home() {
-  await delay(2000);
   return (
     <>
       <ProductList data={sampleData.products} title="New Arrivals" />
